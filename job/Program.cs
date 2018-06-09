f9e3455b0ab01ba5ca9337d1f072e3c192bb9932
Tiago Griffo <tgriffo@thoughtworks.com>
2018-06-09 21:16:56 +0200

    initial commit with project setup

---
+++ job/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace job
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
